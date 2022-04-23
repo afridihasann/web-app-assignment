1fe3b4e1ecb5a8f9c73bc2ea02e946ab0ac64163
Afridi Hasan <ahasan35@myseneca.ca>
2022-04-22 23:25:28 -0400

    commit

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.use((req,res,next) => {
   next();
 });
 
-function ensureLogin = (req,res,next) => {
+function ensureLogin (req,res,next) => {
   if (!(req.session.user)) {
       res.redirect("/login");
   }
